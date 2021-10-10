e969a26b23825fdd04eb2673cd2402059b40e123
Rodrigo Eleuterio <roeleuss@gmail.com>
2021-10-09 23:18:03 -0300

    add essential configs to git install

--- ubuntu20.04-wsl/git-install.sh
+++ ubuntu20.04-wsl/git-install.sh
@@ -4,4 +4,21 @@
 # Instala git no ubuntu20.04 (wsl2)
 ####################################################
 
+if [ -z $1 ]; then
+    echo "First parameter (user.email) is required."
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    echo "Second parameter (user.name) is required."
+    exit 1
+fi
+
 sudo apt install -y git-all
+
+# Configura nome e email do usuário global
+git config --global user.email $1
+git config --global user.name $2
+
+# Configura o git com o email e nome do usuário global, important para salvar o token
+git config --global credential.helper store
